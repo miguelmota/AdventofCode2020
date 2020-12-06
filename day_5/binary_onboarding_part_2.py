2cc4e77d0e505adc3f2efa2650ab40497b8a1ef5
Miguel Mota <miguelmota2@gmail.com>
2020-12-05 21:38:43 -0800

    Add day 5 solution

---
+++ day_5/binary_onboarding_part_2.py
@@ -0,0 +1,52 @@
+import math
+
+def get_seat_id(seat):
+    i = 1
+    x = 0
+    y = 127
+    result = 0
+    for p in seat:
+        if i < 8:
+            if p == 'F':
+                y = math.floor(y - ((y-x)/2))
+            if p == 'B':
+                x = math.ceil(x + ((y-x)/2))
+        if i == 7:
+            if p == 'F':
+                result = x
+            if p == 'B':
+                result = y
+            #print('row = ', result)
+            x = 0
+            y = 7
+        if i >= 8:
+            if p == 'L':
+                y = math.floor(y - ((y-x)/2))
+            if p == 'R':
+                x = math.ceil(x + ((y-x)/2))
+            #print(p, x, y)
+        if i == 10:
+            if p == 'L':
+                column = x
+            if p == 'R':
+                column = y
+            #print('column = ', column)
+            result = result * 8 + column
+        i += 1
+    return result
+
+f = open('input.txt', 'r')
+
+seat_ids = []
+for line in f.readlines():
+    seat = line.strip()
+    seat_ids.append(get_seat_id(seat))
+
+seat_ids = list(set(seat_ids))
+seat_ids.sort(reverse=True)
+print(seat_ids)
+
+for i, sid in enumerate(seat_ids):
+    if sid-1 != seat_ids[i+1]:
+        print(sid)
+        break
